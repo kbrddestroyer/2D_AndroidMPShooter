fa2e0ff165293684f7a1682948781b7e67a27acb
kbrddestroyer <grudko2003@gmail.com>
2023-08-22 16:12:26 +0300

    Added Mirror & Android controls with InputSystem

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
@@ -9,19 +10,35 @@ public class Player : MonoBehaviour
     [Header("Required Objects")]
 
     protected Camera mainCamera;
+    protected PlayerInput playerInput;
+
+    private InputAction move;
+    private InputAction look;
+    private float angle = 0f;
 
     protected void Awake()
     {
         mainCamera = Camera.main;
+        playerInput = GetComponent<PlayerInput>();
+        move = playerInput.actions["Move"];
+        look = playerInput.actions["Look"];
     }
 
     protected void Update()
     {
         // Controls
-        float speedX = Input.GetAxis("Horizontal");
-        float speedY = Input.GetAxis("Vertical");
 
-        transform.position += new Vector3(speedX, speedY, 0) * speed * Time.deltaTime;   // Main moving
+        // 1. Movement controls
+        Vector2 moveControls = move.ReadValue<Vector2>();
 
+        transform.position += new Vector3(moveControls.x, moveControls.y, 0) * speed * Time.deltaTime;   // Main moving
+
+        // 2. Rotation controls
+        if (look.inProgress)
+        {
+            Vector2 lookControls = look.ReadValue<Vector2>();
+            angle = Vector2.Angle(Vector2.up, lookControls) * ((lookControls.x > 0) ? -1 : 1);
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
     }
 }
